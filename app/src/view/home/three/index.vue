7ad85cc5e1e01cc7b14af5cd16567be5f0593681
傅继鹏 <2575048283@qq.com>
2020-12-30 22:44:41 +0800

    feat: 添加立方体动画效果

--- app/src/view/home/three/index.vue
+++ app/src/view/home/three/index.vue
@@ -1,12 +1,49 @@
 <template>
-    <div>
-        你好
-    </div>
+    <div/>
 </template>
 
 <script>
+    import * as THREE from 'three'
     export default {
-        name: "index"
+        name: "index",
+        data(){
+            return{
+                scene:null,
+                cube:null,
+                camera:null,
+                render:null
+            }
+        },
+        created() {
+            this.scene=new THREE.Scene()
+            this.camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+            this.render=new THREE.WebGLRenderer()
+            this.render.setSize(window.innerWidth,window.innerHeight)
+            document.body.appendChild(this.render.domElement)
+        },
+        mounted(){
+            this.createCube()
+            this.animation()
+        },
+        methods: {
+            createCube(){
+                const geometry=new THREE.BoxGeometry()
+                const material=new THREE.MeshBasicMaterial(
+                    {
+                        color:0x00ff00
+                        }
+                    )
+                this.cube=new THREE.Mesh(geometry,material)
+                this.scene.add(this.cube)
+                this.camera.position.z=5
+            },
+            animation(){
+                requestAnimationFrame(this.animation)
+                this.cube.rotation.x+=0.01
+                this.cube.rotation.y+=0.01
+                this.render.render(this.scene,this.camera)
+            }
+        }
     }
 </script>
 
